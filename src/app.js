2f47d5aa59b2b3ed59ad23ff2ecbf02d4f3648f6
Sarah Wood <wood.sarah@outlook.com>
2020-02-07 18:02:07 +0000

    app.js bug fixed - syntax error

--- src/app.js
+++ src/app.js
@@ -6,34 +6,15 @@ import {  BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      list: [
-        ''
-      ]
-    }
-  }
-
-  const App = () => (
-    <BrowserRouter>
+const App = () => (
+  <BrowserRouter>
     <Navbar />
     <Switch>
       <Route exact patjh="/" component={Home} />
     </Switch>
-    </BrowserRouter>
-  )
+  </BrowserRouter>
+)
 
-  render() {
-    return (
-      <div>
-        <h1>Photobook</h1>
-        <p>Some of my favourite snaps</p>
-      </div>
-    )
-  }
-}
 ReactDOM.render(
   <App />,
   document.getElementById('root')
