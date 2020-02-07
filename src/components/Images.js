3017a2990f9fbdd0a89a7f8214c1f6063a389401
Sarah Wood <wood.sarah@outlook.com>
2020-02-07 18:09:02 +0000

    Main images page created - awaiting API

---
+++ src/components/Images.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+
+class Images extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+}
