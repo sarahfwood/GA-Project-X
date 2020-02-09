96018d1a6dc66c0a309a0ca19e8135e2d82636b1
Sarah Wood <wood.sarah@outlook.com>
2020-02-09 00:47:24 +0000

    Images.js update

--- src/components/Images.js
+++ src/components/Images.js
@@ -11,6 +11,19 @@ class Images extends React.Component {
   }
 
   componentDidMount() {
+    axios.get('https://cheesebored.herokuapp.com/cheeses')
+      .then(res => this.setState({ images: res.data }))
+      .catch(err => console.log(err))
+  }
 
+  render() {
+    console.log(this.state.images)
+    return <div>
+      {this.state.images.map((image, i) => {
+        return <ImageCard key={i} image={image}/>
+      })}
+    </div>
   }
 } 
+
+export default Images
