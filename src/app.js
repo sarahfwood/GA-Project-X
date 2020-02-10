c75fe8a7eda983ca3ef79ee3c5758b8399d3368d
Sarah Wood <wood.sarah@outlook.com>
2020-02-10 16:19:58 +0000

    Routes added - Home and Images

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ import {  BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Images from './components/Images'
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Switch>
-      <Route exact patjh="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/images" component={Images}/>
     </Switch>
   </BrowserRouter>
 )
