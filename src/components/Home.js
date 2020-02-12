f36c43422580dff17f6176abeb0e49cee6deee3a
Sarah Wood <wood.sarah@outlook.com>
2020-02-12 18:14:38 +0000

    Links updated to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => (
   <section className="Home">
     <div>
-      <h1>Photobook</h1>
+      <h1><Link to="/">Photobook</Link></h1>
     </div>
     <div>
       <h3>By Sarah Wood</h3>
